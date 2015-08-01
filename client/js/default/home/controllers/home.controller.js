7b60d5fa4dd06ae2755d2689d522bcf5851c34c4
ashish005 <me.ashish005@gmail.com>
2015-08-02 04:26:16 +0530

    small fixes for home page - fund performers changes

--- client/js/default/home/controllers/home.controller.js
+++ client/js/default/home/controllers/home.controller.js
@@ -6,28 +6,30 @@
         app.controller('homeController', ['$scope', '$rootScope', '$http',
             function ($scope, $rootScope, $http)
             {
-                $scope.ngOptions = { data: 'ngData' };
-                /*$scope.ngOptions2 = {
-                    data: 'ngData'
-                };*/
                 $scope.init = function()
                 {
-                    $scope.ngData = [
-                        {Name: "Moroni", Age: 50, Position: 'PR Menager', Status: 'active', Date: '12.12.2014'},
-                        {Name: "Teancum", Age: 43, Position: 'CEO/CFO', Status: 'deactive', Date: '10.10.2014'},
-                        {Name: "Jacob", Age: 27, Position: 'UI Designer', Status: 'active', Date: '09.11.2013'},
-                        {Name: "Nephi", Age: 29, Position: 'Java programmer', Status: 'deactive', Date: '22.10.2014'},
-                        {Name: "Joseph", Age: 22, Position: 'Marketing manager', Status: 'active', Date: '24.08.2013'},
-                        {Name: "Monica", Age: 43, Position: 'President', Status: 'active', Date: '11.12.2014'},
-                        {Name: "Arnold", Age: 12, Position: 'CEO', Status: 'active', Date: '07.10.2013'},
-                        {Name: "Mark", Age: 54, Position: 'Analyst', Status: 'deactive', Date: '03.03.2014'},
-                        {Name: "Amelia", Age: 33, Position: 'Sales manager', Status: 'deactive', Date: '26.09.2013'},
-                        {Name: "Jesica", Age: 41, Position: 'Ruby programmer', Status: 'active', Date: '22.12.2013'},
-                        {Name: "John", Age: 48, Position: 'Marketing manager', Status: 'deactive', Date: '09.10.2014'},
-                        {Name: "Berg", Age: 19, Position: 'UI/UX Designer', Status: 'active', Date: '12.11.2013'}
+                    $scope.performersExtraOptions =
+                    {
+                        alt_asset_class: {title: 'Alternative Asset Class', content: []},
+                        arbitage: {title: 'Arbitrage', content: [], disabled: true},
+                        debt: {title: 'Debt', content: [], disabled: true},
+                        eq_div: {title: 'Equity - Diversified', content: [], disabled: true},
+                        eq_int: {title: 'Equity - International', content: [], disabled: true},
+                        eq_sec: {title: 'Equity - Sectoral', content: [], disabled: true},
+                        gold: {title: 'Gold', content: [], disabled: true},
+                        hybrid: {title: 'Hybrid', content: [], disabled: true}
+                    };
+
+                    $scope.performersInfo = [
+                        {"scheme":"Birla Sun Life Global Commodities Fund - Regular Plan - Growth","Year1Return":-26.06,"latest_nav":10.9870,"date":"30/07/15"},
+                        {"scheme":"DWS Global Agribusiness Offshore Fund - Growth","Year1Return":-3.68,"latest_nav":15.7100,"date":"30/07/15"},
+                        {"scheme":"UTI Wealth Builder Fund - Series II - Growth","Year1Return":4.44,"latest_nav":28.0821,"date":"30/07/15"},
+                        {"scheme":"IDFC Asset Allocation FoF - Conservative - Growth","Year1Return":12.82,"latest_nav":16.3358,"date":"30/07/15"},
+                        {"scheme":"IDFC Asset Allocation FoF - Moderate - Growth","Year1Return":14.03,"latest_nav":17.4346,"date":"30/07/15"}
                     ];
-
-
+                }
+                $scope.getCSSClass = function(value){
+                    return value>=0?'label-primary':'label-warning';
                 }
             }
         ]);
